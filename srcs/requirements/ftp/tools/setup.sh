0868ca312c035ea538ecea0c11777cefe52cb2d4
MliliGenes <saad.elm.77@gmail.com>
2026-03-30 03:02:21 +0100

    sec and env

--- srcs/requirements/ftp/tools/setup.sh
+++ srcs/requirements/ftp/tools/setup.sh
@@ -11,4 +11,8 @@ fi
 echo "${FTP_USER_NAME}:${FTP_USER_PASS}" | chpasswd
 chown -R "${FTP_USER_NAME}:${FTP_USER_NAME}" /var/www/html
 
+mkdir -p /var/run/vsftpd/empty
+chmod 755 /var/run/vsftpd
+chmod 555 /var/run/vsftpd/empty
+
 exec /usr/sbin/vsftpd /etc/vsftpd.conf
